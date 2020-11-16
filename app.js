4d865f53533e3d02778315c71da4d5856f0fb195
Arnaud <arnaud.thevenet@quantrack.io>
2020-11-16 01:23:09 +0100

    new api call

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const request = require('request')
 const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 // const url = 'http://api.weatherstack.com/current?access_key=6de773347ae32a2293b162f0176f7bc0&query=43.651070,-79.347015'
 
@@ -13,10 +14,22 @@ const geocode = require('./utils/geocode')
 //     }
 // })
 
+// geocode('Boston', (error, data) => {
+//     console.log('Error', error)
+//     console.log('Data', data)
+// })
 
+/
+// Goal: Create a reusable function for getting the forecast
+//
+// 1. Setup the "forecast" function in utils/forecast.js
+// 2. Require the function in app.js and call it as shown below
+// 3. The forecast function should have three potential calls to callback:
+//    - Low level error, pass string for error
+//    - Coordinate error, pass string for error
+//    - Success, pass forecast string for data (same format as from before)
 
-
-geocode('Boston', (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
+forecast(-75.7088, 44.1545, (error, data) => {
+  console.log('Error', error)
+  console.log('Data', data)
 })
---
+++ utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=6de773347ae32a2293b162f0176f7bc0&query='+ latitude + ','+ longitude +'&units=f'
+    request({ url: url, json: true}, (error, response) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        }
+        else if (response.body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, response.body.daily.data[0].summary + 'It is currently a test')
+        }
+    })
+}
+
+module.exports = forecast
