38bac61dc8afd8ea5ebf85f4fb87e8e094c1188c
Arnaud <arnaud.thevenet@quantrack.io>
2020-11-17 17:37:28 +0100

    chaining callbacks

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('Boston', (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
+const city = process.argv[2]
+
+geocode(city, (error, data) => {
+  if(city === undefined){
+    console.log('Enter a city.')
+  } else {
+    if (error) {
+      return console.log(error)
+    }
+  
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error)
+      }
+      
+      console.log(data.location)
+      console.log(forecastData)
+    })
+  }
+
 })
 
-forecast(-75.7088, 44.1545, (error, data) => {
-  console.log('Error', error)
-  console.log('Data', data)
-})
+
+
--- utils/forecast.js
+++ utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         else if (response.body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, response.body.daily.data[0].summary + 'It is currently a test')
+            callback(undefined, 'It is currently : ' + response.body.current.temperature + " degree!")
         }
     })
 }
