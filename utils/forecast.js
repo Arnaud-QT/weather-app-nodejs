eefb6a09250bbe2c4d87690a5a3c61e6335f5b53
Arnaud <arnaud.thevenet@quantrack.io>
2020-11-18 22:53:34 +0100

    more objects destructing

--- utils/forecast.js
+++ utils/forecast.js
@@ -2,14 +2,14 @@ const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=6de773347ae32a2293b162f0176f7bc0&query='+ latitude + ','+ longitude +'&units=f'
-    request({ url, json: true}, (error, response) => {
+    request({ url, json: true}, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         }
-        else if (response.body.error) {
+        else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, 'It is currently : ' + response.body.current.temperature + " degree!")
+            callback(undefined, 'It is currently : ' + body.current.temperature + " degree!")
         }
     })
 }
